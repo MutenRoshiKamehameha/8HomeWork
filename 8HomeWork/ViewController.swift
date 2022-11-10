65b9005b48bcfa5cb4c6fff53923d0100f524c0e
baygeldi <akylbekuulubaygeldi@gmail.com>
2022-11-10 14:07:32 +0600

    ok

--- 8HomeWork/ViewController.swift
+++ 8HomeWork/ViewController.swift
@@ -9,6 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBOutlet weak var textField2: UITextField!
+    
+    var result = 0
+    
+    
+    
+    
+    @IBAction func plusButton(_ sender: UIButton) {
+        result = Int(textField.text!)! + Int(textField2.text!)!
+        resultLabel.text = String(result)
+    }
+    
+    
+    @IBAction func minusButton(_ sender: UIButton) {
+        
+        result = Int(textField.text!)! - Int(textField2.text!)!
+        resultLabel.text = String(result)
+        
+    }
+    
+    
+    @IBAction func umnojitButton(_ sender: UIButton) {
+        
+        result = Int(textField.text!)! * Int(textField2.text!)!
+        resultLabel.text = String(result)
+        
+    }
+    
+    
+    @IBAction func razdelitButton(_ sender: UIButton) {
+    
+    result = Int(textField.text!)! / Int(textField2.text!)!
+    resultLabel.text = String(result)
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +61,17 @@ class ViewController: UIViewController {
 
 }
 
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
